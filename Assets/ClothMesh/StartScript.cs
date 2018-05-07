8efb285a10c147c079e517264e0b70741927f54f
TravisO <ottt0794@my.uwstout.edu>
2018-05-06 22:51:38 -0500

    Cloth simulation working

---
+++ Assets/ClothMesh/StartScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour {
+
+    MeshCloth clothTest = new MeshCloth();
+	// Use this for initialization
+	void Start () {
+        clothTest = gameObject.AddComponent(typeof(MeshCloth)) as MeshCloth;
+
+        clothTest.addCollider(Vector3.zero, 7f);
+    }
+    int i = 0;
+	// Update is called once per frame
+	void FixedUpdate () {
+        clothTest.Simulate();        
+    }
+}
